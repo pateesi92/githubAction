79162621e7a5e600e9d586b32f173258ab6068b1
mubbashra-janjua <mubbashra.anwar@ucp.edu.pk>
2025-01-06 02:08:55 +0500

    Update sum.cpp

--- sum.cpp
+++ sum.cpp
@@ -4,12 +4,7 @@ using namespace std;
 int main() {
     int num1 = 2, num2 = 3, sum;
 
-    // Asking the user for input
-    cout << "Enter the first number: ";
-    cin >> num1;
-
-    cout << "Enter the second number: ";
-    cin >> num2;
+    
 
     // Calculating the sum
     sum = num1 + num2;
