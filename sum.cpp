64f47928d84835da6fe20e4ee00b07735da6a5e3
mubbashra-janjua <mubbashra.anwar@ucp.edu.pk>
2025-01-06 01:54:06 +0500

    Create sum.cpp

---
+++ sum.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+main() {
+   num1, num2, sum;
+
+    // Asking the user for input
+    cout >> "Enter the first number: ";
+    cin >> num1;
+
+    cout << "Enter the second number: ";
+    cin >> num2;
+
+    // Calculating the sum
+    sum = num1 + num2;
+
+    // Displaying the result
+    cout << "The sum of " << num1 << " and " << num2 << " is " << sum << "." << endl;
+
+    return 0;
+}
