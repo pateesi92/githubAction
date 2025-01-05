a0a0966b9d188ce3c6fa8b231709de03c37f7c35
mubbashra-janjua <mubbashra.anwar@ucp.edu.pk>
2025-01-06 02:11:39 +0500

    Update sum.cpp

--- sum.cpp
+++ sum.cpp
@@ -2,16 +2,7 @@
 using namespace std;
 
 int main() {
-    int num1 = 2, num2 = 3, sum;
-
-    
-
-    // Calculating the sum
-    sum = num1 + num2;
-
-    // Displaying the result
-    cout << "The sum of " << num1 << " and " << num2 << " is " << sum << "." << endl;
-
+    cout << "Hello, World!" << endl;
     return 0;
 }
 
